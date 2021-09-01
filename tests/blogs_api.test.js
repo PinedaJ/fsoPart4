bd63ac66777c89c4d99135645da15773fe0f4d92
PinedaJ <jeff.c.pineda@gmail.com>
2021-09-01 15:39:31 -0400

    exercises 4.13-4.14

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -14,102 +14,140 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('making an HTTP POST req to /api/blogs creates a new blog post', async () => {
-  const newBlog = {
-    title: "TDD harms architecture 2",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html",
-    likes: 2
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain(
-    'TDD harms architecture 2'
-  )
-
-  const authors = blogsAtEnd.map(b => b.author)
-  expect(authors).toContain(
-    'Robert C. Martin'
-  )
-
-  const urls = blogsAtEnd.map(b => b.url)
-  expect(urls).toContain(
-    'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html'
-  )
-
-  const likess = blogsAtEnd.map(b => b.likes)
-  expect(likess).toContain(
-    2
-  )
-})
-
-test('blog without likes is given a value of 0', async () => {
-  const newBlog = {
-    title: "TDD harms architecture 3",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html",
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const likess = blogsAtEnd.map(b => b.likes)
-  expect(likess[helper.initialBlogs.length]).toBe(0)
-})
-
-test('blog without title or url is not added', async () => {
-  const newBlogNoTitle = {
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html",
-  }
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
   
-  const newBlogNoUrl = {
-    title: "TDD harms architecture 3",
-    author: "Robert C. Martin",
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoTitle)
-    .expect(400)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
   
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoUrl)
-    .expect(400)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+})
 
-  const blogsAtEnd = await helper.blogsInDb()
+describe('addition of a new blog', () => {
+  test('making an HTTP POST req to /api/blogs creates a new blog post', async () => {
+    const newBlog = {
+      title: "TDD harms architecture 2",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html",
+      likes: 2
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain(
+      'TDD harms architecture 2'
+    )
+  
+    const authors = blogsAtEnd.map(b => b.author)
+    expect(authors).toContain(
+      'Robert C. Martin'
+    )
+  
+    const urls = blogsAtEnd.map(b => b.url)
+    expect(urls).toContain(
+      'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html'
+    )
+  
+    const likess = blogsAtEnd.map(b => b.likes)
+    expect(likess).toContain(
+      2
+    )
+  })
+  
+  test('blog without likes is given a value of 0', async () => {
+    const newBlog = {
+      title: "TDD harms architecture 3",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html",
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const likess = blogsAtEnd.map(b => b.likes)
+    expect(likess[helper.initialBlogs.length]).toBe(0)
+  })
+  
+  test('blog without title or url is not added', async () => {
+    const newBlogNoTitle = {
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture-2.html",
+    }
+  
+    const newBlogNoUrl = {
+      title: "TDD harms architecture 3",
+      author: "Robert C. Martin",
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlogNoTitle)
+      .expect(400)
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlogNoUrl)
+      .expect(400)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+})
 
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+  
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+  
+    const titles = blogsAtEnd.map(r => r.title)
+  
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+})
+
+describe('updating an individual blog post', () => {
+  test('changes that number of likes of a post', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    blogToUpdate.likes = 1234
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .expect(200)
+    
+    expect(blogsAtStart[0].likes).toBe(1234)
+  })
 })
 
 afterAll(() => {
